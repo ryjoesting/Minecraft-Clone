83e76bfd79b082a561667ebd516b77574b440cf2
Ryan Joesting <nsd-rjoesting@nucor.com>
2023-10-19 15:49:22 -0500

    added ReadJSON method in block.cpp

--- MinecraftClone/src/block.cpp
+++ MinecraftClone/src/block.cpp
@@ -1,12 +1,18 @@
+#include <cstdio>
+#include <iostream>
+#include "rapidjson/document.h" 
+#include "rapidjson/filereadstream.h" 
+#include "rapidjson/rapidjson.h"
 #include "block.hpp"
 #include "glfw/glfw3.h"
 #include "vendor/stb_image/stb_image.h"
 
-Block::Block(BlockID id) {
+Block::Block(BlockID id, glm::vec3 position) : ID(id), Position(position) {
+	rapidjson::Document doc;
 	switch (id)
 	{
 	case GRASS:
-
+		doc.Parse()
 		break;
 	case DIRT:
 		break;
@@ -21,4 +27,31 @@ Block::Block(BlockID id) {
 	default:
 		break;
 	}
+}
+rapidjson::Document Block::ReadJSON(std::string filepath) {
+    FILE* fp = fopen(filepath.c_str(), "rb");
+    if (!fp) {
+        std::cerr << "Error: unable to open file"
+            << std::endl;
+        return NULL;
+    }
+
+    char readBuffer[1024];
+    rapidjson::FileReadStream is(fp, readBuffer,
+        sizeof(readBuffer));
+
+    // Parse the JSON document 
+    rapidjson::Document doc;
+    doc.ParseStream(is);
+
+    // Check if the document is valid 
+    if (doc.HasParseError()) {
+        std::cerr << "Error: failed to parse JSON document"
+            << std::endl;
+        fclose(fp);
+        return NULL;
+    }
+
+    // Close the file 
+    fclose(fp);
 }
--- MinecraftClone/src/block.hpp
+++ MinecraftClone/src/block.hpp
@@ -1,12 +1,18 @@
 #pragma once
-
+#include <glm/glm.hpp>
+#include <rapidjson/document.h>
+#include <iostream>
 enum BlockID { GRASS, DIRT, GRAVEL, COBBLESTONE, OAK_LOG, OAK_PLANKS };
 
 class Block {
 private:
-	float vertices[180];
+	BlockID ID;
+	float Vertices[180];
+	glm::vec3 Position;
+
+	rapidjson::Document ReadJSON(std::string filepath);
 public:
-	Block(BlockID id);
+	Block(BlockID id, glm::vec3 position);
 
 
 };
